237892f39afa60053077e5f531b660f1530ee72a
Bijan Roy <bijanr2004@gmail.com>
2025-11-21 11:30:48 +0530

    minor changes

--- weather/script.js
+++ weather/script.js
@@ -6,5 +6,14 @@ const output = document.getElementById("output-box");
 
 const apikey = "e9413d66e57315b811138420d35cfc21";
 
-btn.addEventListener("click", getWeather());
+btn.addEventListener("click",getWeather);
 
+
+function getWeather(){
+    const city = input.value;
+    
+    const url = 'https://api.openweathermap.org/data/3.0/onecall?lat=22.57&lon=88.36&appid={e9413d66e57315b811138420d35cfc21}'
+
+    const 
+
+}
