8a3fc0ed98404696e7147f9076efc39f50787a08
Bijan Roy <bijanr2004@gmail.com>
2025-11-21 15:46:35 +0530

    updated

--- weather/script.js
+++ weather/script.js
@@ -3,7 +3,8 @@ const input = document.getElementById('input');
 const btn = document.getElementById("btn");
 
 const output = document.getElementById("output-box");
-
+ 
+// login into www.weatherapi.com and create an API key
 const apikey = "db5c0a64548144cdb16104957252505";
 
 btn.addEventListener("click",getWeather);
@@ -16,7 +17,15 @@ async function getWeather(){
 
     const data = await fetch(url);
 
-    const value = data.json;
+    const value = await data.json();
 
-    console.log(value)
+    // output.innerHTML(`<p>${value.current.temp_c}</p>`)
+    // or you can just use innerText (to show just text). innerHTML is used to add elements along with text or any kind of data.
+    // store the temperture and country in two variables (highly not recommended for efficiency purposes)
+    const temp = value.current.temp_c;
+    const country = value.location.country;
+    // you can write this as
+    // output.innerText = temp;  // or
+    output.innerText = `It's ${temp}°C in ${city}, ${country}`;
+    // this will be shown as It's 28°C in Kolkata, India
 }
